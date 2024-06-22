ff0be0caeac279cc470720fe43e4aa58c8900fff
mooose22 <mostafamoawad22@gmail.com>
2024-06-22 16:15:28 -0400

    added rating fix

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom'
-import Header from './Components/Header/Header';
-import Home from './Pages/Home';
-import MovieList from './Components/Movie/MovieList';
-import MovieDetail from './Pages/MovieDetail';
-// import Header from './Components/Header';
-// import Home from './Pages/Home';
-// import MovieDetail from './Pages/MovieDetail';
-// import MovieList from './Components/MovieList';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header/Header";
+import Home from "./Pages/Home";
+import MovieList from "./Components/Movie/MovieList";
+import MovieDetail from "./Pages/MovieDetail";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <Router>
         <Header />
         <Routes>
           <Route index element={<Home />} />
-          <Route path='movie/:id' element={<MovieDetail />} />
-          <Route path='movies/:type' element={<MovieList />} />
+          <Route path="movie/:id" element={<MovieDetail />} />
+          <Route path="movies/:type" element={<MovieList />} />
         </Routes>
       </Router>
     </div>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,45 +1,56 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import './Home.css'
-import { Link } from 'react-router-dom';
-import AliceCarousel from 'react-alice-carousel';
-import Search from '../Components/Search/Search'
+import React from "react";
+import { useState, useEffect } from "react";
+import "./Home.css";
+import { Link } from "react-router-dom";
+import AliceCarousel from "react-alice-carousel";
+import Search from "../Components/Search/Search";
 
 const Home = () => {
-
   const [popular, setPopular] = useState([]);
 
   useEffect(() => {
-    window.scroll(0, 0)
-    fetchData()
-  }, [])
+    window.scroll(0, 0);
+    fetchData();
+  }, []);
 
   function fetchData() {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=dfac79bf601a1c8766457c868e28203e&language=en-US`)
-      .then(res => res.json())
-      .then(data => setPopular(data.results))
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=dfac79bf601a1c8766457c868e28203e&language=en-US`
+    )
+      .then((res) => res.json())
+      .then((data) => setPopular(data.results));
   }
 
-  const items = popular.map(movie => (
-    <Link style={{ textDecoration: "none", color: "white" }} to={`/movie/${movie.id}`} >
+  const items = popular.map((movie) => (
+    <Link
+      style={{ textDecoration: "none", color: "white" }}
+      to={`/movie/${movie.id}`}
+    >
       <div className="main-slider-parent">
         {
           <>
             <div className="slider-image">
-              <img src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`} alt={movie.title} className='slider-main-img' />
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                alt={movie.title}
+                className="slider-main-img"
+              />
             </div>
             <div className="slider-image-details">
-              <h1 className='slider-title'>{movie.original_title || movie.name}</h1>
+              <h1 className="slider-title">
+                {movie.original_title || movie.name}
+              </h1>
               <br />
-              <h2 className='slider-vote'>{movie.vote_average} <i className="fas fa-star" />{" "}</h2>
+              <h2 className="slider-vote">
+                {movie.vote_average.toFixed(1)} <i className="fas fa-star" />{" "}
+              </h2>
               <p>{movie.overview}</p>
             </div>
           </>
         }
       </div>
     </Link>
-  ))
-
+  ));
 
   return (
     <>
@@ -56,8 +67,7 @@ const Home = () => {
       />
       <Search />
     </>
-  )
+  );
+};
 
-}
-
-export default Home
+export default Home;
